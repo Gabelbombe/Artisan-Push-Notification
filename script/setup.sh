b670eecd8a586c0de7e1976e144ef202bcf4406b
ehime <dodomeki@gmail.com>
2015-10-02 08:28:52 -0700

    Dir moving asking hosts if we exist

--- script/setup.sh
+++ script/setup.sh
@@ -4,3 +4,9 @@
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as su" 1>&2 ; exit 1
 fi
+
+if [ ! -z "$(grep 'artisan.io' /etc/hosts)" ]; then
+  echo -e "127.0.0.1\tartisan.io\twww.artisan.io" >> /etc/hosts
+fi
+
+exit 0 ## good
